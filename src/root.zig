2b56dd08c82299ed833d84902676fd2dc9e8c874
Navid M <navidmomtahen@gmail.com>
2026-03-26 00:12:44 +0000

    remove unused function

--- src/root.zig
+++ src/root.zig
@@ -6,11 +6,3 @@ pub const Email = email.Email;
 pub const EmailHeader = email.EmailHeader;
 pub const Attachment = email.Attachment;
 pub const parseEmail = email.parseEmail;
-
-pub fn bufferedPrint() !void {
-    var stdout_buffer: [1024]u8 = undefined;
-    var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
-    const stdout = &stdout_writer.interface;
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-    try stdout.flush();
-}
